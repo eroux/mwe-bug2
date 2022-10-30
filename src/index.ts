33e321aa6e37955292c98ae8c730aed79e7f8f2a
eroux <elie.roux@telecom-bretagne.eu>
2022-10-30 14:29:20 +0100

    still works (surprisingly)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 
 export { default as NewEntityContainer } from "./containers/NewEntityContainer"
 
-export { EntityGraph } from "./helpers/rdf/types"
+export { defaultGraphNode } from "./helpers/rdf/types"
 
