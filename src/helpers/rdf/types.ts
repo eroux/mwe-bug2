7c7e605d6345403a7533e2ccd078bdd166c0d6d0
eroux <elie.roux@telecom-bretagne.eu>
2022-10-30 14:10:03 +0100

    still works

--- src/helpers/rdf/types.ts
+++ src/helpers/rdf/types.ts
@@ -150,115 +150,6 @@ export class EntityGraphValues {
     this.subjectUri = subjectUri
   }
 
-  onGetInitialValues = (subjectUri: string, pathString: string, values: Array<Value>) => {
-    if (!(subjectUri in this.oldSubjectProps)) this.oldSubjectProps[subjectUri] = {}
-    if (!(subjectUri in this.newSubjectProps)) this.newSubjectProps[subjectUri] = {}
-    this.oldSubjectProps[subjectUri][pathString] = values
-    this.newSubjectProps[subjectUri][pathString] = values
-  }
-
-  onUpdateValues = (subjectUri: string, pathString: string, values: Array<Value>) => {
-    if (!(subjectUri in this.newSubjectProps)) this.newSubjectProps[subjectUri] = {}
-    this.newSubjectProps[subjectUri][pathString] = values
-    // disable history for current modification (autocreation of new empty simple value)
-    if (this.noHisto === true) {
-      this.noHisto = false
-      return
-    }
-    updateHistory(this.subjectUri, subjectUri, pathString, values, this.noHisto)
-    // there are some modifications that are chained and we need only first not to be stored
-    // (case of creation of new property with subproperties)
-    if (this.noHisto === 1) this.noHisto = -1
-  }
-
-  isInitialized = (subjectUri: string, pathString: string) => {
-    return subjectUri in this.oldSubjectProps && pathString in this.oldSubjectProps[subjectUri]
-  }
-
-  addNewValuestoStore(store: rdf.Store, subjectUri: string) {
-    if (!(subjectUri in this.newSubjectProps)) return
-    const subject = new rdf.NamedNode(subjectUri)
-    for (const pathString in this.newSubjectProps[subjectUri]) {
-      // handling inverse path vs. direct path
-      if (pathString.startsWith("^")) {
-        const property = new rdf.NamedNode(pathString.substring(1))
-        const values: Array<Value> = this.newSubjectProps[subjectUri][pathString]
-        for (const val of values) {
-          if (val instanceof LiteralWithId) {
-            throw "can't add literals in inverse path, something's wrong with the data!"
-          } else {
-            if (val.node?.value == "tmp:uri" || val.node?.value == "tmp:none") continue
-            store.add(val.node, property, subject, defaultGraphNode)
-            if (val instanceof Subject) {
-              this.addNewValuestoStore(store, val.uri)
-            }
-          }
-        }
-      } else {
-        const listMode = pathString.endsWith("[]")
-        const property = new rdf.NamedNode(listMode ? pathString.substring(0, pathString.length - 2) : pathString)
-        const values: Array<Value> = this.newSubjectProps[subjectUri][pathString]
-        const collection = new rdf.Collection()
-        for (const val of values) {
-          //debug("val:", val, listMode)
-          if (val instanceof LiteralWithId) {
-            // do not add empty strings
-            if (val.value == "") continue
-            if (listMode) collection.append(val)
-            else store.add(subject, property, val, defaultGraphNode)
-          } else {
-            //debug("saving:", val)
-            if (val.node?.value == "tmp:uri" || val.node?.value == "tmp:none") continue
-            if (listMode) {
-              // val.node happens to be undefined when list has been updated in UI
-              if (val.node) {
-                collection.append(val.node)
-              }
-              // TODO: ???
-              else if (val instanceof rdf.Literal) {
-                collection.append(val)
-              } else throw "could not add " + val + " to collection " + collection
-            } else store.add(subject, property, val.node, defaultGraphNode)
-            if (val instanceof Subject) {
-              this.addNewValuestoStore(store, val.uri)
-            }
-          }
-        }
-        if (listMode && collection.elements.length) {
-          collection.close()
-          store.add(subject, property, collection, defaultGraphNode)
-        }
-      }
-    }
-  }
-
-  propsUpdateEffect: (subjectUri: string, pathString: string) => AtomEffect<Array<Value>> =
-    (subjectUri: string, pathString: string) =>
-    ({ setSelf, onSet }: setSelfOnSelf) => {
-      onSet((newValues: Array<Value> | DefaultValue): void => {
-        if (!(newValues instanceof DefaultValue)) {
-          //debug("updating:",subjectUri, pathString, newValues)
-          this.onUpdateValues(subjectUri, pathString, newValues)
-        }
-      })
-    }
-
-  @Memoize((pathString: string, subjectUri: string) => {
-    return subjectUri + pathString
-  })
-  getAtomForSubjectProperty(pathString: string, subjectUri: string) {
-    return atom<Array<Value>>({
-      key: this.idHash + subjectUri + pathString,
-      default: [],
-      effects_UNSTABLE: [this.propsUpdateEffect(subjectUri, pathString)],
-      // disable immutability in production
-      dangerouslyAllowMutability: true,
-    })
-  }
-
-  hasSubject(subjectUri: string): boolean {
-    return subjectUri in this.newSubjectProps
-  }
 }
 
 type setSelfOnSelf = {
@@ -268,8 +159,6 @@ type setSelfOnSelf = {
 
 // a proxy to an EntityGraph that updates the entity graph but is purely read-only, so that React is happy
 export class EntityGraph {
-  onGetInitialValues: (subjectUri: string, pathString: string, values: Array<Value>) => void
-  getAtomForSubjectProperty: (pathString: string, subjectUri: string) => RecoilState<Array<Value>>
 
   getValues: () => EntityGraphValues
 
@@ -303,90 +192,9 @@ export class EntityGraph {
     // the freeze doesn't proagate to it
     const values = new EntityGraphValues(topSubjectUri)
     this.topSubjectUri = topSubjectUri
-    this.onGetInitialValues = values.onGetInitialValues
-    this.getAtomForSubjectProperty = (pathString, subjectUri) =>
-      values.getAtomForSubjectProperty(pathString, subjectUri)
     this.connexGraph = connexGraph
     this.getValues = () => {
       return values
-    }
-  }
-
-  addNewValuestoStore(store: rdf.Store): void {
-    this.values.addNewValuestoStore(store, this.topSubjectUri)
-  }
-
-  static addIdToLitList = (litList: Array<rdf.Literal>): Array<LiteralWithId> => {
-    return litList.map((lit: rdf.Literal): LiteralWithId => {
-      return new LiteralWithId(lit.value, lit.language, lit.datatype)
-    })
-  }
-
-  static addLabelsFromGraph = (resList: Array<rdf.NamedNode>, graph: EntityGraph): Array<RDFResourceWithLabel> => {
-    return resList.map((res: rdf.NamedNode): RDFResourceWithLabel => {
-      return new RDFResourceWithLabel(res, graph)
-    })
-  }
-
-  hasSubject(subjectUri: string): boolean {
-    if (this.values.hasSubject(subjectUri)) return true
-    return this.store.any(new rdf.NamedNode(subjectUri), null, null) != null
-  }
-
-  static subjectify = (resList: Array<rdf.NamedNode>, graph: EntityGraph): Array<Subject> => {
-    return resList.map((res: rdf.NamedNode): Subject => {
-      return new Subject(res, graph)
-    })
-  }
-
-  // only returns the values that were not initalized before
-  getUnitializedValues(s: RDFResource, p: any): Array<Value> | null {
-    const path = p.path
-    if (!path) return null
-    if (this.values.isInitialized(s.uri, path.sparqlString)) {
-      return null
-    }
-    return this.getPropValuesFromStore(s, p)
-  }
-
-  getPropValuesFromStore(s: RDFResource, p: any): Array<Value> {
-    if (!p.path) {
-      throw "can't find path of " + p.uri
-    }
-    switch (p.objectType) {
-      case ObjectType.Internal:
-        const fromRDFSubNode: Array<rdf.NamedNode> = s.getPropResValuesFromPath(p.path)
-        const fromRDFSubs = EntityGraph.subjectify(fromRDFSubNode, s.graph)
-        this.onGetInitialValues(s.uri, p.path.sparqlString, fromRDFSubs)
-        return fromRDFSubs
-        break
-      case ObjectType.ResInList:
-        if (!p.path.directPathNode) {
-          throw "can't have non-direct path for property " + p.uri
-        }
-        const fromRDFResList: Array<rdf.NamedNode> = s.getPropResValues(p.path.directPathNode)
-        // TODO: p.graph should be the graph of the ontology instead
-        const fromRDFReswLabels = EntityGraph.addLabelsFromGraph(fromRDFResList, p.graph)
-        this.onGetInitialValues(s.uri, p.path.sparqlString, fromRDFReswLabels)
-        return fromRDFReswLabels
-        break
-      case ObjectType.Literal:
-      case ObjectType.LitInList:
-      default:
-        if (!p.path.directPathNode) {
-          throw "can't have non-direct path for property " + p.uri
-        }
-        let fromRDFLits: Array<rdf.Literal>
-        if (p.hasListAsObject) {
-          const fromRDFLitsList = s.getPropLitValuesFromList(p.path.directPathNode)
-          fromRDFLits = fromRDFLitsList === null ? [] : fromRDFLitsList
-        } else {
-          fromRDFLits = s.getPropLitValues(p.path.directPathNode)
-        }
-        const fromRDFLitIDs = EntityGraph.addIdToLitList(fromRDFLits)
-        this.onGetInitialValues(s.uri, p.path.sparqlString, fromRDFLitIDs)
-        return fromRDFLitIDs
-        break
     }
   }
 }
@@ -426,113 +234,6 @@ export class RDFResource {
     return this.node.value
   }
 
-  static valuesByLang(values: Array<Value>): Record<string, string> {
-    const res: Record<string, string> = {}
-    for (const value of values) {
-      if (value instanceof LiteralWithId) {
-        res[value.language] = value.value
-      }
-    }
-    return res
-  }
-
-  public getPropValueByLang(p: rdf.NamedNode): Record<string, string> {
-    if (this.node instanceof rdf.Collection) return {}
-    const lits: Array<rdf.Literal> = this.graph.store.each(this.node, p, null) as Array<rdf.Literal>
-    const res: Record<string, string> = {}
-    for (const lit of lits) {
-      res[lit.language] = lit.value
-    }
-    return res
-  }
-
-  public getPropValueOrNullByLang(p: rdf.NamedNode): Record<string, string> | null {
-    if (this.node instanceof rdf.Collection) return {}
-    const lits: Array<rdf.Literal> = this.graph.store.each(this.node, p, null) as Array<rdf.Literal>
-    const res: Record<string, string> = {}
-    let i = 0
-    for (const lit of lits) {
-      i += 1
-      res[lit.language] = lit.value
-    }
-    if (i == 0) return null
-    return res
-  }
-
-  public getPropLitValues(p: rdf.NamedNode): Array<rdf.Literal> {
-    if (this.node instanceof rdf.Collection) return []
-    return this.graph.store.each(this.node, p, null) as Array<rdf.Literal>
-  }
-
-  public getPropResValues(p: rdf.NamedNode): Array<rdf.NamedNode> {
-    if (this.node instanceof rdf.Collection) return []
-    return this.graph.store.each(this.node, p, null) as Array<rdf.NamedNode>
-  }
-
-  public getPropResValuesFromList(p: rdf.NamedNode): Array<rdf.NamedNode> | null {
-    if (this.node instanceof rdf.Collection) return null
-    const colls = this.graph.store.each(this.node, p, null) as Array<rdf.Collection>
-    for (const coll of colls) {
-      return coll.elements as Array<rdf.NamedNode>
-    }
-    return null
-  }
-
-  public getPropLitValuesFromList(p: rdf.NamedNode): Array<rdf.Literal> | null {
-    if (this.node instanceof rdf.Collection) return null
-    const colls = this.graph.store.each(this.node, p, null) as Array<rdf.Collection>
-    for (const coll of colls) {
-      return coll.elements as Array<rdf.Literal>
-    }
-    return null
-  }
-
-  public getPropIntValue(p: rdf.NamedNode): number | null {
-    if (this.node instanceof rdf.Collection) return null
-    const lit: rdf.Literal | null = this.graph.store.any(this.node, p, null) as rdf.Literal | null
-    if (lit === null) return null
-    return rdfLitAsNumber(lit)
-  }
-
-  public getPropStringValue(p: rdf.NamedNode): string | null {
-    if (this.node instanceof rdf.Collection) return null
-    const lit: rdf.Literal | null = this.graph.store.any(this.node, p, null) as rdf.Literal | null
-    if (lit === null) return null
-    return lit.value
-  }
-
-  public getPropResValue(p: rdf.NamedNode): rdf.NamedNode | null {
-    if (this.node instanceof rdf.Collection) return null
-    const res: rdf.NamedNode | null = this.graph.store.any(this.node, p, null) as rdf.NamedNode | null
-    return res
-  }
-
-  public getPropResValuesFromPath(p: Path): Array<rdf.NamedNode> {
-    if (this.node instanceof rdf.Collection) return []
-    if (p.directPathNode) {
-      return this.graph.store.each(this.node, p.directPathNode, null) as Array<rdf.NamedNode>
-    }
-    return this.graph.store.each(null, p.inversePathNode, this.node) as Array<rdf.NamedNode>
-  }
-
-  public getPropResValueFromPath(p: Path): rdf.NamedNode | null {
-    if (this.node instanceof rdf.Collection) return null
-    if (p.directPathNode) {
-      return this.graph.store.any(this.node, p.directPathNode, null) as rdf.NamedNode | null
-    }
-    return this.graph.store.any(this.node, p.inversePathNode, null) as rdf.NamedNode | null
-  }
-
-  public getPropBooleanValue(p: rdf.NamedNode, dflt = false): boolean {
-    if (this.node instanceof rdf.Collection) return dflt
-    const lit: rdf.Literal = this.graph.store.any(this.node, p, null) as rdf.Literal
-    if (!lit) return dflt
-    const n = Boolean(lit.value)
-    if (n) {
-      return n
-    }
-    return dflt
-  }
 }
 
 export class RDFResourceWithLabel extends RDFResource {
@@ -566,13 +267,5 @@ export class Subject extends RDFResource {
   constructor(node: rdf.NamedNode, graph: EntityGraph) {
     super(node, graph)
     this.node = node
-  }
-
-  getUnitializedValues(property: any): Array<Value> | null {
-    return this.graph.getUnitializedValues(this, property)
-  }
-
-  getAtomForProperty(pathString: string) {
-    return this.graph.getAtomForSubjectProperty(pathString, this.uri)
   }
 }
